8cbbab09f01e08479bd534dd521700d1b3005fb9
root <root@localhost.localdomain>
2026-08-04 19:01:57 +0000

    fix: alinha s_star com a thread pública (9,25%)
    - Reverte commits que mudaram para 6,25%
    - Remove etanol 42% e biodiesel 35% (sem fonte confirmada)
    - Atualiza README para refletir 9,25%

--- src/selix/core.py
+++ src/selix/core.py
@@ -52,6 +52,8 @@ class SELIX:
         self.inflacao = inflacao or 4.48
         self.roe = roe or 31.23
         self.selic_bacen = selic_bacen
+        self.etanol_mix = 0.27  # 27% (valor oficial)
+        self.biodiesel_mix = 0.15  # 15% (valor oficial)
         self.teto_juro_real = self.inflacao + self.JURO_REAL_MAXIMO
         self.teto_roe = self.roe * self.FOLGA_ROE
         self.teto_global = (self.RELACAO_GLOBAL * self.inflacao) + self.PREMIO_RISCO_BRASIL
